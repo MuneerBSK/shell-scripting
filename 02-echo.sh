743a1dfe9576b9f09c335494f8a299903b604b34
MuneerBSK <muneerbsk96@gmail.com>
2023-12-26 18:37:02 -0500

    AutoPush

---
+++ 02-echo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo line1
+echo line2
+echo line3
+
+# How to print multiple lines using single echo command?
+     # to do so we need to use escape sequence characters
+  #   \n : It telss the ssytem to move the cursor to the next line
+  #   \t : It moves the cursor to a tab space and then prints the next line
+
+
+  echo line1\nline2
