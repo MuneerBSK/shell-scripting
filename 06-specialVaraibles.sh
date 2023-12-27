3d8a4e33b1520f5cd1033e38ee045b2f65b6682d
MuneerBSK <muneerbsk96@gmail.com>
2023-12-26 20:42:35 -0500

    AutoPush

--- 06-specialVaraibles.sh
+++ 06-specialVaraibles.sh
@@ -11,3 +11,5 @@ echo "Name of the script is : $0"     # Gives the name of the script you're runn
 echo first value is $1
 echo second value is $2 
 echo third value is $3
+
+# bash scriptName.sh 100 200 300
