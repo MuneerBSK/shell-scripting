fe62466828f4a12aab83097a0cf602d37983a3f3
MuneerBSK <muneerbsk96@gmail.com>
2023-12-26 19:55:42 -0500

    AutoPush

--- 04-variables.sh
+++ 04-variables.sh
@@ -18,3 +18,10 @@ echo ${a}
 echo "$c"
 
 echo "I am printing the value of d $d"
+
+# When you try to print a variable which is not declared, bash is going to consider that as NULL or EMPTY
+
+rm -rf /data/${DATA_DIR}   # /data/test ----> rm -rf /data/
+
+# How do you supply variables from the command line 
+# export varName =  value 
