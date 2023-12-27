4ed47de97c22c014390419cd61655af73e24a8ef
MuneerBSK <muneerbsk96@gmail.com>
2023-12-27 14:38:24 -0500

    AutoPush

--- 12-case.sh
+++ 12-case.sh
@@ -14,14 +14,18 @@ case $ACTION in
 
      start)
          echo -e "\e[32m starting payment service \e[0m"
+         exit 0
          ;;
      stop)
          echo -e "\e[34m Stopping Payment service \e[0m"
+         exit 2
          ;;
      restart)
          echo -e "\e[35m Restarting payment service \e[0m"
+         exit 3
          ;;
      *)
-         echo -e "Valid options are \e[32m start or stop or restart \e[0m"
+         echo -e "Valid options are \e[35m start or stop or restart \e[0m"
+         exit 4
          ;;
 esac
