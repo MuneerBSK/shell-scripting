6b9c1f9b0be4f4daadcda54e772a13dae789129b
MuneerBSK <muneerbsk96@gmail.com>
2023-12-27 14:23:51 -0500

    AutoPush

--- 12-case.sh
+++ 12-case.sh
@@ -13,12 +13,12 @@ ACTION=$1
 case $ACTION in
 
      start)
-         echo "starting payment service"
+         echo -e "\e[32m starting payment service \e[0m"
          ;;
      stop)
-         echo "Stopping Payment service"
+         echo -e "\e[34m Stopping Payment service \e[0m"
          ;;
      restart)
-         echo "Restarting payment service"
+         echo -e "\e[35m Restarting payment service \e[0m"
          ;;
 esac
