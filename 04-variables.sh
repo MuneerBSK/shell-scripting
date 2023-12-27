de1a8bb5e9c288eafcd5ea4f4c695676790a82d3
MuneerBSK <muneerbsk96@gmail.com>
2023-12-26 19:50:11 -0500

    AutoPush

---
+++ 04-variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# What is a variable? A variable is something which holds some value dynamically.
+
+a=10
+b=20
+c=30
+
+# no data types are avaailable in Basha by default
+# Everything is considered as STRING
+ 
+# How do you print a variable?
+# We iuse a special character, called $ to print.
+# echo $a
+
+echo $A
+echo $(a)
+echo "$c"
