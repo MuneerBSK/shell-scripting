dc25f773cea814e43b857574024a99e3f5a35908
MuneerBSK <muneerbsk96@gmail.com>
2023-12-26 20:14:20 -0500

    AutoPush

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TODAYDATE="26-Dec-2023"
+TODAYDATE=$(date +%F) # This wqy of declaring is called hardcoding.
 
 echo -e "welcome to Bash training, todays date is \e[32m ${TODAYDATE} \e[0m]"
 
