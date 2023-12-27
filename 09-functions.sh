c314b19cf92c4642b1fe6edd3b0b52effcf5f48d
MuneerBSK <muneerbsk96@gmail.com>
2023-12-26 21:08:57 -0500

    AutoPush

--- 09-functions.sh
+++ 09-functions.sh
@@ -6,3 +6,9 @@
 # 2) Aliases                  ( Alises are shortcuts,  alias net="netstat -tulpn" )
 # 3) Shell Built-in Commands  
 # 4) Functions                # Functions are nothing but a set of command that can be written in a sequence and can be called n number of times as per your choice.
+
+# Declaring a function 
+
+sample() {
+    echo "I am a messaged called from sample function"
+}
