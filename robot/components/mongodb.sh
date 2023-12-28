a5bfce155047101636b083b8377a6b9498e991a8
MuneerBSK <muneerbsk96@gmail.com>
2023-12-27 21:46:51 -0500

    AutoPush

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 
-```bash
-# curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
-```
+set -e
 
-Install Mongo & Start Service.
+# validating whether the executed user is root user or not
+if [ "$EUID" -ne 0 ] ; then
+    echo -e "\e[31m You should execute it as root user or with a sudo prefix \e[0m"
+    exit 1
+fi
 
-```bash
-# yum install -y mongodb-org
-# systemctl enable mongod
-# systemctl start mongod
-```
+COMPONENT="mongo"
+LOGFILE="/tmp/$COMPONENT.log"
+
+stat () {
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m $2: Success \e[0m"
+    else
+        echo -e "\e[31m $2: Failure \e[0m"
+        exit 2
+    fi 
+}
+
+echo -n "Configuring the $COMPONENT repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
+
+echo -n "Installing $COMPONENT"
+yum install -y mongodb-org   &>> $LOGFILE
+stat $?
+
+
+
+systemctl enable mongod
+systemctl start mongod
+
 Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
 
 Config file:   `# vim /etc/mongod.conf`
 
-```bash
+
 systemctl restart mongod
-```
 
-  
 
-- Every Database needs the schema to be loaded for the application to work.
-
----
-
-      `Download the schema and inject it.`
-
-```
 # curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
 
 # cd /tmp
@@ -35,4 +48,3 @@ systemctl restart mongod
 # cd mongodb-main
 # mongo < catalogue.js
 # mongo < users.js
-```
