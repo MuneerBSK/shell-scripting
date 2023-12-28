0d466a9f70164934f24569c4c2f73c873966369c
MuneerBSK <muneerbsk96@gmail.com>
2023-12-27 21:04:34 -0500

    AutoPush

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -17,20 +17,51 @@ if [ $? -eq 0 ] ; then
     echo -e "\e[32m Success \e[0m"
 else
     echo -e "\e[31m Failure \e[0m"
+    exit 2
 fi
 
+echo -n "Downloading the frontend component :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+    exit 2
+fi
 
+echo -n "Performing cleanup old frontend content: "
 cd /usr/share/nginx/html
 rm -rf *    &>> /tmp/frontend.log
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+    exit 2
+fi
+
+echo -n "Copying the downloaded frontend content: "
 unzip /tmp/frontend.zip    &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+    exit 2
+fi
 
+echo -n "Starting the service: "
 systemctl enable nginx    &>> /tmp/frontend.log
 systemctl start nginx     &>> /tmp/frontend.log
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+    exit 2
+fi
+
 
 # Here are my 3 step observations :
 
