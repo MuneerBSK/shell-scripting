1289c1be900498f270ed41f9b22003e87088cde6
MuneerBSK <muneerbsk96@gmail.com>
2023-12-27 22:25:55 -0500

    AutoPush

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -1,43 +1,45 @@
-#!/bin/bash
+#!/bin/bash 
 
-# set -e
+# set -e 
 
 COMPONENT=mongodb
-source components/common.sh
+source components/common.sh  
+
 
 echo -n "Configuring the $COMPONENT repo :"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
-stat $? "Configuring the $COMPONENT repo"
+stat $? 
 
 echo -n "Installing $COMPONENT :"
-yum install -y mongodb-org &>>$LOGFILE
-stat $? "Installing $COMPONENT"
+yum install -y mongodb-org  &>> $LOGFILE
+stat $? 
+
 
 echo -n "Starting $COMPONENT :"
-systemctl enable mongod &>>$LOGFILE
-systemctl start mongod &>>$LOGFILE
-stat $? "Starting $COMPONENT"
+systemctl enable mongod     &>> $LOGFILE
+systemctl start mongod      &>> $LOGFILE
+stat $? 
 
 echo -n "Updating the $COMPONENT visibility : "
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-stat $? "Updating the $COMPONENT visibility"
+stat $? 
 
 echo -n "Performing Daemon-Reload : "
-systemctl daemon-reload &>>$LOGFILE
+systemctl daemon-reload  &>> $LOGFILE
 systemctl restart mongod 
-stat $? "Performing Daemon-Reload"
+stat $?
 
-echo -n "Downloading the $COMPONENT schema :"
+echo -n "Downloading the  $COMPONENT schema :"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-stat $? "Downloading the $COMPONENT schema"
+stat $? 
 
 echo -n "Extracting the $COMPONENT schema : "
 cd /tmp 
-unzip -o mongodb.zip &>>$LOGFILE
-stat $? "Extracting the $COMPONENT schema"
+unzip -o $COMPONENT.zip  &>> $LOGFILE
+stat $? 
 
 echo -n "Injecting the schema :"
 cd /tmp/$COMPONENT-main
-mongo < catalogue.js &>>$LOGFILE
-mongo < users.js &>>$LOGFILE
-stat $? "Injecting the schema"
+mongo < catalogue.js    &>> $LOGFILE
+mongo < users.js        &>> $LOGFILE
+stat $? 
