8d02c502d6eea4b13bace057500010fb3499b51a
MuneerBSK <muneerbsk96@gmail.com>
2023-12-27 20:41:10 -0500

    AutoPush

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -12,8 +12,12 @@ fi
 
 echo "installing ngnix : "
 yum install nginx -y  &>> /tmp/frontend.log
+
 if [ $? -eq 0 ] ; then
-    echo -e "\e[32m "Success \e[0m"
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+fi
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
