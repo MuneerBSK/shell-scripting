7b3acb900212297d0f18c44f583a3ac2daaad97a
MuneerBSK <muneerbsk96@gmail.com>
2023-12-28 14:38:25 -0500

    AutoPush

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -24,7 +24,7 @@ stat () {
 }
 
 echo -n "Configuring the nodejs repo :"
-yum install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
+yum --silent install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
 stat $?
 
 echo -n "Installing NodeJS :"
