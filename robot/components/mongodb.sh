c0c28085bedf718e0066352b9eec5ad9cd8e2552
MuneerBSK <muneerbsk96@gmail.com>
2023-12-28 10:04:06 -0500

    AutoPush

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -22,17 +22,18 @@ stat () {
     fi 
 }
 
-echo -n "Configuring the $COMPONENT repo"
+echo -n "Configuring the $COMPONENT repo :"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 stat $?
 
-echo -n "Installing $COMPONENT"
+echo -n "Installing $COMPONENT :"
 yum install -y mongodb-org  &>> LOGFILE
 stat $?
 
-
-# systemctl enable mongod
-# systemctl start mongod
+echo -n "Starting $COMPONENT :"
+systemctl enable mongod   &>> LOGFILE
+systemctl start mongod    &>> LOGFILE
+stat $?
 
 # Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
 
