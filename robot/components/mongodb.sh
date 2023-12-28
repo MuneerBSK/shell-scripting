84333f9e2c982995dc12abc0205983e1ce9129da
MuneerBSK <muneerbsk96@gmail.com>
2023-12-27 22:17:49 -0500

    AutoPush

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -1,52 +1,45 @@
-#!/bin/bash
+#!/bin/bash 
 
-set -e
+# set -e 
 
-# validating whether the executed user is root user or not
-if [ "$EUID" -ne 0 ]; then
-    echo -e "\e[31m You should execute it as root user or with a sudo prefix \e[0m"
-    exit 1
-fi
+COMPONENT=mongodb
+source components/common.sh  
 
-COMPONENT="mongo"
-LOGFILE="/tmp/$COMPONENT.log"
 
-stat() {
-    if [ $1 -eq 0 ]; then
-        echo -e "\e[32m $2: Success \e[0m"
-    else
-        echo -e "\e[31m $2: Failure \e[0m"
-        exit 2
-    fi
-}
-
-echo "Configuring the $COMPONENT repo"
+echo -n "Configuring the $COMPONENT repo :"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
-stat $? "Configuring the $COMPONENT repo"
+stat $? 
 
-echo "Installing $COMPONENT :"
-yum install -y mongodb-org &>>$LOGFILE
-stat $? "Installing $COMPONENT"
+echo -n "Installing $COMPONENT :"
+yum install -y mongodb-org  &>> $LOGFILE
+stat $? 
 
-echo "Starting $COMPONENT :"
-systemctl enable mongod &>>$LOGFILE
-systemctl start mongod &>>$LOGFILE
-stat $? "Starting $COMPONENT"
 
-echo "Updating the $COMPONENT visibility : "
+echo -n "Starting $COMPONENT :"
+systemctl enable mongod     &>> $LOGFILE
+systemctl start mongod      &>> $LOGFILE
+stat $? 
+
+echo -n "Updating the $COMPONENT visibility : "
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-stat $? "Updating the $COMPONENT visibility"
+stat $? 
 
-echo "Performing Daemon-reload : "
-systemctl daemon-reload &>>$LOGFILE
-systemctl restart mongod &>>$LOGFILE
-stat $? "Performing Daemon-reload"
+echo -n "Performing Daemon-Reload : "
+systemctl daemon-reload  &>> $LOGFILE
+systemctl restart mongod 
+stat $?
 
+echo -n "Downloading the  $COMPONENT schema :"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $? 
 
-# # curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+echo -n "Extracting the $COMPONENT schema : "
+cd /tmp 
+unzip -o $COMPONENT.zip  &>> $LOGFILE
+stat $? 
 
-# # cd /tmp
-# # unzip mongodb.zip
-# # cd mongodb-main
-# # mongo < catalogue.js
-# # mongo < users.js
+echo -n "Injecting the schema :"
+cd /tmp/$COMPONENT-main
+mongo < catalogue.js    &>> $LOGFILE
+mongo < users.js        &>> $LOGFILE
+stat $? 
