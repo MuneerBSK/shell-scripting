cce1e9dac9eb192293bbefd9783d4b012980fcb7
MuneerBSK <muneerbsk96@gmail.com>
2023-12-27 21:23:19 -0500

    AutoPush

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -2,57 +2,46 @@
 
 set -e
 
-COMPONENT= "frontend"
-LOGFILE= "/tmp/$COMPONENT.log"
-
 # validating whether the executed user is root user or not
-
-ID=$(id -u)
-if [ "$ID" -ne 0 ] ; then
+if [ "$EUID" -ne 0 ] ; then
     echo -e "\e[31m You should execute it as root user or with a sudo prefix \e[0m"
     exit 1
 fi
 
+COMPONENT="frontend"
+LOGFILE="/tmp/$COMPONENT.log"
+
 stat () {
-     if [ $1 -eq 0 ] ; then
-        echo -e "\e[32m Success \e[0m"
-     else
-        echo -e "\e[31m Failure \e[0m"
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m $2: Success \e[0m"
+    else
+        echo -e "\e[31m $2: Failure \e[0m"
         exit 2
-     fi 
+    fi 
 }
 
 echo -n "Installing nginx : "
-yum install nginx -y  &>> $LOGFILE
-stat $?
-
+yum install nginx -y &>> $LOGFILE
+stat $? "Installing nginx"
 
 echo -n "Downloading the $COMPONENT component :"
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-stat $?
+stat $? "Downloading the $COMPONENT component"
 
-echo -n "Performing cleanup old $COMPONENT content : "
+echo -n "Performing cleanup of old $COMPONENT content : "
 cd /usr/share/nginx/html
-rm -rf *    &>> $LOGFILE
-stat $?
+rm -rf * &>> $LOGFILE
+stat $? "Performing cleanup of old $COMPONENT content"
 
 echo -n "Copying the downloaded $COMPONENT content : "
-unzip /tmp/$COMPONENT.zip    &>> $LOGFILE
+unzip /tmp/$COMPONENT.zip &>> $LOGFILE
 mv $COMPONENT-main/* .
 mv static/* .
 rm -rf $COMPONENT-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-stat $?
+stat $? "Copying the downloaded $COMPONENT content"
 
 echo -n "Starting the service : "
-systemctl enable nginx    &>> $LOGFILE
-systemctl start nginx     &>> $LOGFILE
-stat $?
-
-
-
-# Here are my 3 step observations :
-
-# 1. Few of the steps are failed, still my script executed irrespective of the failure. : set -e
-# 2. Instalaltion failed, because i have not validated that i have a root privileges
-# 3. The information i would like to provided is like success or failure.
+systemctl enable nginx &>> $LOGFILE
+systemctl start nginx &>> $LOGFILE
+stat $? "Starting the service"
