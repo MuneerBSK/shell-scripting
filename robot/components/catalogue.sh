d991e574eb4d0452639e09c4b479460668f180cb
MuneerBSK <muneerbsk96@gmail.com>
2023-12-29 11:13:15 -0500

    AutoPush

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -59,7 +59,7 @@ npm install   &>> $LOGFILE
 stat $?
 
 echo -n "Updating the systemd file with DB details :"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'  /home/$APPUSER/$COMPONENT/systemd.service
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
 mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
 stat $?
 
