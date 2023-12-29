27c00b13e1e921e546a562e469b286b3f4808da8
MuneerBSK <muneerbsk96@gmail.com>
2023-12-29 18:34:33 -0500

    AutoPush

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -23,7 +23,7 @@ stat () {
 }
 
 echo -n "Configuring $COMPONENT repo :"
-curl -L https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo -o /etc/yum.repos.d/$COMPONENT.repo
+curl -L https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo -o /etc/yum.repos.d/$COMPONENT.repo    &>> $LOGFILE
 stat $?
 
 echo -n "Installing $COMPONENT server :"
