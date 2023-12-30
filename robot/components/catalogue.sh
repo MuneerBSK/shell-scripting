50c3929fcba76f83217bae3e59e688cd46afbbbb
MuneerBSK <muneerbsk96@gmail.com>
2023-12-29 22:34:33 -0500

    AutoPush

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -1,17 +1,6 @@
-#!/bin/bash
+#!/bin/bash 
 
-set -e
+COMPONENT=catalogue
+source components/common.sh    # Source is going to load the file, so that you can call all of them as per your need
 
-# validating whether the executed user is root user or not
-ID=$(id -u)
-
-if [ "$ID" -ne 0 ] ; then
-    echo -e "\e[31m You should execute it as root user or with a sudo prefix \e[0m"
-    exit 1
-fi
-
-COMPONENT="catalogue"
-LOGFILE="/tmp/$COMPONENT.log"
-APPUSER=roboshop
-
-
+NODEJS                         # Calling NodeJS Function.
