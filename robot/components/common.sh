ba05c894d504977c1e4cdc642c1bc178349363b1
MuneerBSK <muneerbsk96@gmail.com>
2023-12-29 23:08:27 -0500

    AutoPush

--- robot/components/common.sh
+++ robot/components/common.sh
@@ -30,7 +30,7 @@ CREATE_USER() {
 }
 
 
-DOWNLOADING_AND_EXTRACT () {
+DOWNLOAD_AND_EXTRACT () {
 
     echo -n "Downloading the $COMPONENT component :"
     curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
