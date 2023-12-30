2946117707eedfba3bd9ed7019537e3edf79b2d9
MuneerBSK <muneerbsk96@gmail.com>
2023-12-30 15:05:07 -0500

    AutoPush

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -16,40 +16,40 @@ systemctl enable mysqld  &>> $LOGFILE
 systemctl start mysqld   &>> $LOGFILE  
 stat $?
 
-echo -n "Grab $COMPONENT default password :"
-DEFAULT_ROOT_PWD=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
-stat $? 
+# echo -n "Grab $COMPONENT default password :"
+# DEFAULT_ROOT_PWD=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
+# stat $? 
 
-# This should only run for the first time or when the default password is not changed.
-echo "show databases;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE 
-if [ $? -ne 0 ] ; then 
+# # This should only run for the first time or when the default password is not changed.
+# echo "show databases;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE 
+# if [ $? -ne 0 ] ; then 
 
-    echo -n "Password Reset of root user :"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE 
-    stat $?
+#     echo -n "Password Reset of root user :"
+#     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE 
+#     stat $?
 
-fi 
+# fi 
 
-# Ensure you run this only when the password validate plugin exist 
-echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password  &>> $LOGFILE 
-if [ $? -eq 0 ] ; then 
+# # Ensure you run this only when the password validate plugin exist 
+# echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password  &>> $LOGFILE 
+# if [ $? -eq 0 ] ; then 
 
-    echo -n "Uninstalling Password Validation Plugin :"
-    echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE
-    stat $?
+#     echo -n "Uninstalling Password Validation Plugin :"
+#     echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE
+#     stat $?
 
-fi 
+# fi 
 
-echo -n "Downloading the $COMPONENT Schema :"
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> $LOGFILE
-stat $? 
+# echo -n "Downloading the $COMPONENT Schema :"
+# curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> $LOGFILE
+# stat $? 
 
-echo -n "Extracting the $COMPONENT Schema :"
-cd /tmp 
-unzip -o /tmp/$COMPONENT.zip &>> $LOGFILE
-stat $? 
+# echo -n "Extracting the $COMPONENT Schema :"
+# cd /tmp 
+# unzip -o /tmp/$COMPONENT.zip &>> $LOGFILE
+# stat $? 
 
-echo -n "Injecting the schema :"
-cd $COMPONENT-main
-mysql -uroot -pRoboShop@1 < shipping.sql 
-stat $? 
+# echo -n "Injecting the schema :"
+# cd $COMPONENT-main
+# mysql -uroot -pRoboShop@1 < shipping.sql 
+# stat $? 
