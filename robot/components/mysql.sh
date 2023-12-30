85a32b7b23be3d8a0e34f8ebb199b7e401fdda82
MuneerBSK <muneerbsk96@gmail.com>
2023-12-30 15:24:14 -0500

    AutoPush

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -16,19 +16,19 @@ systemctl enable mysqld  &>> $LOGFILE
 systemctl start mysqld   &>> $LOGFILE  
 stat $?
 
-# echo -n "Grab $COMPONENT default password :"
-# DEFAULT_ROOT_PWD=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
-# stat $? 
+echo -n "Grab $COMPONENT default password :"
+DEFAULT_ROOT_PWD=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
+stat $? 
 
-# # This should only run for the first time or when the default password is not changed.
-# echo "show databases;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE 
-# if [ $? -ne 0 ] ; then 
+# This should only run for the first time or when the default password is not changed.
+echo "show databases;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE 
+if [ $? -ne 0 ] ; then 
 
-#     echo -n "Password Reset of root user :"
-#     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE 
-#     stat $?
+    echo -n "Password Reset of root user :"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE 
+    stat $?
 
-# fi 
+fi 
 
 # # Ensure you run this only when the password validate plugin exist 
 # echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password  &>> $LOGFILE 
