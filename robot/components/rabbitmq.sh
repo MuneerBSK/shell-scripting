5b4f4bb5250069253dc615f401d8cb5933f1a4ae
MuneerBSK <muneerbsk96@gmail.com>
2023-12-30 10:30:09 -0500

    AutoPush

--- robot/components/rabbitmq.sh
+++ robot/components/rabbitmq.sh
@@ -7,23 +7,23 @@ COMPONENT=rabbitmq
 source components/common.sh
 
 echo -n "Installing ERlang dependency"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash  &>> $LOGFILE
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash  &>> $LOGFILE
+curl -s https://packagecloud.io/install/repositories/$COMPONENT/erlang/script.rpm.sh | sudo bash  &>> $LOGFILE
+curl -s https://packagecloud.io/install/repositories/$COMPONENT/erlang/script.rpm.sh | sudo bash  &>> $LOGFILE
 stat $?
 
 echo -n "Installing $COMPONENT :"
-yum install rabbitmq-server -y   &>> $LOGFILE
+yum install $COMPONENT-server -y   &>> $LOGFILE
 stat $?
 
 echo -n"Starting $COMPONENT :"
-systemctl enable rabbitmq-server   &>> $LOGFILE
-systemctl start rabbitmq-server    &>> $LOGFILE
+systemctl enable $COMPONENT-server   &>> $LOGFILE
+systemctl start $COMPONENT-server    &>> $LOGFILE
 stat $?
 
 echo -n "Creating $COMPONENT application user :"
 rabbitmqctl add_user roboshop roboshop123             &>> $LOGFILE
 rabbitmqctl set_user_tags roboshop administrator      &>> $LOGFILE
-
+stat $?
 
 
 # # 
