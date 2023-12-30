f32e3050ec2b1334ef6e8b8ae5357605ad22aff5
MuneerBSK <muneerbsk96@gmail.com>
2023-12-29 22:52:34 -0500

    AutoPush

--- robot/components/common.sh
+++ robot/components/common.sh
@@ -1,46 +1,46 @@
 LOGFILE="/tmp/$COMPONENT.log"
 APPUSER=roboshop
 
-# Validting whether the executed user is a root user or not 
+# validating whether the executed user is root user or not
 ID=$(id -u)
 
-if [ "$ID" -ne 0 ] ; then 
-    echo -e "\e[31m You should execute this script as a root user or with a sudo as prefix \e[0m" 
+if [ "$ID" -ne 0 ] ; then
+    echo -e "\e[31m You should execute it as root user or with a sudo prefix \e[0m"
     exit 1
-fi 
+fi
 
-
-stat() {
-    if [ $1 -eq 0 ] ; then 
-        echo -e "\e[32m Success \e[0m"
-    else 
-        echo -e "\e[31m Failure \e[0m"
+stat () {
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m $2: Success \e[0m"
+    else
+        echo -e "\e[31m $2: Failure \e[0m"
         exit 2
     fi 
 }
 
 CREATE_USER() {
 
-    id $APPUSER  &>> $LOGFILE
-    if [ $? -ne 0 ] ; then 
-        echo -n "Creating the Application User Account :" 
-        useradd roboshop &>> $LOGFILE
-        stat $? 
-    fi 
+      id $APPUSER
+      if [ $? -ne 0 ]; then
+            echo -n "Creating the application user account :"
+            useradd $APPUSER    &>> $LOGFILE
+            stat $? 
+      fi
 
 }
 
-DOWNLOAD_AND_EXTRACT() {
+
+DOWNLOADING_AND_EXTRACT () {
 
     echo -n "Downloading the $COMPONENT component :"
     curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-    stat $? 
+    stat $?
 
-    echo -n "Extracting the $COMPONENT in the $APPUSER directory"
-    cd /home/$APPUSER 
-    rm -rf /home/$APPUSER/$COMPONENT &>> $LOGFILE
-    unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE
-    stat $? 
+    echo -n "Extracting the $COMPONENT in the $APPUSER directory:"
+    cd /home/$APPUSER
+    rm -rf /home/$APPUSER/$COMPONENT  &>> $LOGFILE
+    unzip -o /tmp/$COMPONENT.zip   &>> $LOGFILE
+    stat $?
 
     echo -n "Configuring the permissions :"
     mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
@@ -49,26 +49,25 @@ DOWNLOAD_AND_EXTRACT() {
 
 }
 
-NPM_INSTALL() {
-
-    echo -n "Installing the $COMPONENT Application :"
-    cd /home/$APPUSER/$COMPONENT/ 
-    npm install &>> $LOGFILE
-    stat $? 
+NPM_INSTALL () {
+    echo -n "Installing the $COMPONENT application :"
+    cd /home/$APPUSER/$COMPONENT
+    npm install   &>> $LOGFILE
+    stat $?
 
 }
 
 CONFIG_SVC() {
 
-    echo -n "Updating the systemd file with DB Details :"
-    sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/'  -e  's/CARTHOST/cart.roboshop.internal/' -e  's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+    echo -n "Updating the systemd file with DB details :"
+    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
     mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-    stat $? 
+    stat $?
 
-    echo -n "Starting the $COMPONENT service : "
-    systemctl daemon-reload &>> $LOGFILE
+    echo -n "Starting the $COMPONENT service :"
+    systemctl daemon-reload     &>> $LOGFILE
     systemctl enable $COMPONENT &>> $LOGFILE
-    systemctl restart $COMPONENT &>> $LOGFILE
+    systemctl start $COMPONENT  &>> $LOGFILE
     stat $?
 
 }
@@ -76,23 +75,26 @@ CONFIG_SVC() {
 NODEJS() {
 
     echo -n "Configuring the nodejs repo :"
-    curl --silent --location https://rpm.nodesource.com/setup_16.x | bash - &>> $LOGFILE
-    stat $?  
-
-    echo -n "Installing NodeJS :"
-    yum install nodejs -y &>> $LOGFILE
+    curl --silent --location https://rpm.nodesource.com/setup_16.x | bash -  &>> $LOGFILE
     stat $?
 
-    # Calling Create-User Functon 
+    echo -n "Installing NodeJS :"
+    yum install nodejs -y    &>> $LOGFILE
+    stat $?
+
+    # Calling create-user function
     CREATE_USER
 
     # Calling Download_And_Extract Function
     DOWNLOAD_AND_EXTRACT
 
-    # Calling NPM Install Function
+    # Calling NPM install function
     NPM_INSTALL
-
+    
     # Calling Config-Svc Function
     CONFIG_SVC
 
 }
+
+
+
