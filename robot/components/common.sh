7db82448bbee09638e87e6a01b935f69aaeda184
MuneerBSK <muneerbsk96@gmail.com>
2023-12-29 22:33:44 -0500

    AutoPush

--- robot/components/common.sh
+++ robot/components/common.sh
@@ -18,8 +18,83 @@ stat () {
     fi 
 }
 
+CREATE_USER() {
+
+      id $APPUSER
+      if [ $? -ne 0 ]; then
+            echo -n "Creating the application user account :"
+            useradd $APPUSER    &>> $LOGFILE
+            stat $? 
+      fi
+
+}
+
+
+DOWNLOADING_AND_EXTRACT () {
+
+    echo -n "Downloading the $COMPONENT component :"
+    curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+    stat $?
+
+    echo -n "Extracting the $COMPONENT in the $APPUSER directory:"
+    cd /home/$APPUSER
+    rm -rf /home/$APPUSER/$COMPONENT  &>> $LOGFILE
+    unzip -o /tmp/$COMPONENT.zip   &>> $LOGFILE
+    stat $?
+
+    echo -n "Configuring the permissions :"
+    mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
+    chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
+    stat $?
+
+}
+
+NPM_INSTALL () {
+    echo -n "Installing the $COMPONENT application :"
+    cd /home/$APPUSER/$COMPONENT
+    npm install   &>> $LOGFILE
+    stat $?
+
+}
+
+CONFIG_SVC() {
+
+    echo -n "Updating the systemd file with DB details :"
+    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+    mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+    stat $?
+
+    echo -n "Starting the $COMPONENT service :"
+    systemctl daemon-reload     &>> $LOGFILE
+    systemctl enable $COMPONENT &>> $LOGFILE
+    systemctl start $COMPONENT  &>> $LOGFILE
+    stat $?
+
+}
+
 NODEJS() {
+
     echo -n "Configuring the nodejs repo :"
     curl --silent --location https://rpm.nodesource.com/setup_16.x | bash -  &>> $LOGFILE
     stat $?
+
+    echo -n "Installing NodeJS :"
+    yum install nodejs -y    &>> $LOGFILE
+    stat $?
+
+    # Calling create-user function
+    CREATE_USER
+
+    # Calling Download_And_Extract Function
+    DOWNLOAD_AND_EXTRACT
+
+    # Calling NPM install function
+    NPM_INSTALL
+    
+    # Calling Config-Svc Function
+    CONFIG_SVC
+
 }
+
+
+
