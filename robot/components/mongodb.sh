6d6ed6b5eec21bb7d298fce1c40fe9f04e6aa7cd
MuneerBSK <muneerbsk96@gmail.com>
2024-01-15 19:47:41 -0500

    AutoPush

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -35,26 +35,26 @@ systemctl enable mongod     &>> $LOGFILE
 systemctl start mongod      &>> $LOGFILE
 stat $? 
 
-echo -n "Updating the $COMPONENT visibility : "
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-stat $? 
+# echo -n "Updating the $COMPONENT visibility : "
+# sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+# stat $? 
 
-echo -n "Performing Daemon-Reload : "
-systemctl daemon-reload  &>> $LOGFILE
-systemctl restart mongod 
-stat $?
+# echo -n "Performing Daemon-Reload : "
+# systemctl daemon-reload  &>> $LOGFILE
+# systemctl restart mongod 
+# stat $?
 
-echo -n "Downloading the  $COMPONENT schema :"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-stat $? 
+# echo -n "Downloading the  $COMPONENT schema :"
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+# stat $? 
 
-echo -n "Extracting the $COMPONENT schema : "
-cd /tmp 
-unzip -o $COMPONENT.zip  &>> $LOGFILE
-stat $? 
+# echo -n "Extracting the $COMPONENT schema : "
+# cd /tmp 
+# unzip -o $COMPONENT.zip  &>> $LOGFILE
+# stat $? 
 
-echo -n "Injecting the schema :"
-cd /tmp/$COMPONENT-main
-mongo < catalogue.js    &>> $LOGFILE
-mongo < users.js        &>> $LOGFILE
-stat $? 
+# echo -n "Injecting the schema :"
+# cd /tmp/$COMPONENT-main
+# mongo < catalogue.js    &>> $LOGFILE
+# mongo < users.js        &>> $LOGFILE
+# stat $? 
