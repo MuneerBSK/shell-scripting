aa3a5ccd13021ef2e5c91db12adf6aeb406657c0
MuneerBSK <muneerbsk96@gmail.com>
2024-01-15 20:10:29 -0500

    AutoPush

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -23,11 +23,11 @@ stat() {
 }
 
 echo -n "Configuring the $COMPONENT repo :"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/mongo.repo
 stat $? 
 
 echo -n "Installing $COMPONENT :"
-yum install -y mongodb-org  &>> $LOGFILE
+yum install -y $COMPONENT-org  &>> $LOGFILE
 stat $? 
 
 echo -n "Starting $COMPONENT :"
@@ -45,16 +45,16 @@ systemctl restart mongod
 stat $?
 
 echo -n "Downloading the  $COMPONENT schema :"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 stat $? 
 
-# echo -n "Extracting the $COMPONENT schema : "
-# cd /tmp 
-# unzip -o $COMPONENT.zip  &>> $LOGFILE
-# stat $? 
+echo -n "Extracting the $COMPONENT schema : "
+cd /tmp 
+unzip -o $COMPONENT.zip  &>> $LOGFILE
+stat $? 
 
-# echo -n "Injecting the schema :"
-# cd /tmp/$COMPONENT-main
-# mongo < catalogue.js    &>> $LOGFILE
-# mongo < users.js        &>> $LOGFILE
-# stat $? 
+echo -n "Injecting the schema :"
+cd /tmp/$COMPONENT-main
+mongo < catalogue.js    &>> $LOGFILE
+mongo < users.js        &>> $LOGFILE
+stat $? 
