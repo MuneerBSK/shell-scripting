9992b2bd1451cf539fa7e1517e55dbbd8b27b76e
MuneerBSK <muneerbsk96@gmail.com>
2024-01-15 20:45:49 -0500

    AutoPush

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -31,9 +31,12 @@ echo -n "Installing nodejs :"
 yum install nodejs -y   &>> $LOGFILE
 stat $?
 
-echo -n "Creating application user account :"
-useradd $APPUSER   &>> $LOGFILE
-stat $?
+id $APPUSER
+if [ $? -ne 0 ] ; then
+    echo -n "Creating application user account :"
+    useradd $APPUSER   &>> $LOGFILE
+    stat $?
+fi
 
 echo -n "Downloading the $COMPONENT component :"
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
