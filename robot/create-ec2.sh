e501ef0e6d364e34c7b37cb6e0299f5177763134
MuneerBSK <muneerbsk96@gmail.com>
2024-01-20 19:57:15 -0500

    AutoPush

---
+++ robot/create-ec2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+# This is a script created to launch EC2 Servers and create the associated Route53 Record 
+
+aws ec2 describe-images --region us-east-1 --image-ids ami-0f75a13ad2e340a58 | jq '.Images[].ImageId'
+echo -n "Ami ID is $AMI_ID"
+
+echo -n "Launching the instance with $AMI_ID as AMI :"
