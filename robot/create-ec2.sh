242cc7db2fd49c26737877f9713c73250959f38e
MuneerBSK <muneerbsk96@gmail.com>
2024-01-22 10:05:46 -0500

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -2,6 +2,13 @@
 
 # This is a script created to launch EC2 Servers and create the associated Route53 Record 
 
+if [ -z "$1" ] || [ -z "$2" ]; then 
+    echo -e "\e[31m Component Name is required \e[0m \t\t"
+    echo -e "\t\t\t \e[32m Sample Usage is : $ bash create-ec2.sh user dev \e[0m"
+    exit 1
+fi 
+
+COMPONENT=$1
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo -n "Ami ID is $AMI_ID"
