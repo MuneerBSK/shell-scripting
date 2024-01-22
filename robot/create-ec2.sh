c01200762cb6d334cdca73f0382eb17fec2b8252
MuneerBSK <muneerbsk96@gmail.com>
2024-01-22 10:21:38 -0500

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -1,17 +1,18 @@
-#!/bin/bash 
+#!/bin/bash
 
-# This is a script created to launch EC2 Servers and create the associated Route53 Record 
+# This is a script created to launch EC2 Servers and create the associated Route53 Record
 
-if [ -z "$1" ] ; then 
+if [ -z "$1" ]; then
     echo -e "\e[31m Component Name is required \e[0m \t\t"
     echo -e "\t\t\t \e[32m Sample Usage is : $ bash create-ec2.sh user \e[0m"
     exit 1
-fi 
+fi
 
 COMPONENT=$1
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
-echo -n "Ami ID is $AMI_ID"
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq -r '.Images[].ImageId')
+echo "Ami ID is $AMI_ID"
 
-echo -n "Launching the instance with $AMI_ID as AMI :"
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=Instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
+echo "Launching the instance with $AMI_ID as AMI :"
+instance_id=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq -r '.Instances[].InstanceId')
+echo "Instance ID is $instance_id"
