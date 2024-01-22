13c3e5d8b57d59c9a9b6c76ca5e5ebed4fd3e562
MuneerBSK <muneerbsk96@gmail.com>
2024-01-22 10:13:28 -0500

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -14,4 +14,4 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Cen
 echo -n "Ami ID is $AMI_ID"
 
 echo -n "Launching the instance with $AMI_ID as AMI :"
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=Instance,Tags=[{Key=Name,value+$COMPONENT}]" | jq
